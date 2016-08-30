85e3f3bf1d61bdbdb1d94f2de2c8e8af939814ea
Dan Fockler <dfockler13@gmail.com>
2016-08-30 10:53:03 -0700

    simple option parsing

--- lib/pietr.rb
+++ lib/pietr.rb
@@ -2,11 +2,10 @@ require 'pietr/interpreter'
 require 'pietr/image'
 require 'pietr/color'
 require 'pietr/ops'
-require 'byebug'
 
 module Pietr
-  def self.run
-    image = Pietr::Image.new("assets/piet_factorial.png")
+  def self.run(filename)
+    image = Pietr::Image.new(filename)
     interpreter = Pietr::Interpreter.new(image)
     interpreter.run
   end
